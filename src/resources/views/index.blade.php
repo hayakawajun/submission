c36cbba44bb09cc2c99976ceb5ced0a0310b844b
hayakawa jun <hayakawajugon@gmail.com>
2025-09-01 16:56:57 +0900

    html作成中

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection
+
+@section('content')
+      <div class="contact-form__content">
+      <div class="contact-form__heading">
+        <h2>Contact</h2>
+      </div>
+      <form class="form" action="/contacts/confirm" method="post">
+        @csrf
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お名前</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input class="form__input--last-name" type="text" name="" placeholder="例: 山田" value="{{ old('name') }}" />
+              <input class="form__input--first-name" type="text" name="" placeholder="例: 太郎" value="{{ old('name') }}" />
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">メールアドレス</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input class="form__input--address" type="email" name="email" placeholder="例: test@example.com" value="{{ old('email') }}" />
+            </div>
+            <div class="form__error">
+              @error('email')
+              {{ $message }}
+              @enderror
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">電話番号</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input class="form__input-tel" type="tel" name="tel" placeholder="090" value="{{ old('tel') }}" />
+              <input class="form__input-tel" type="tel" name="tel" placeholder="090" value="{{ old('tel') }}" />
+              <input class="form__input-tel" type="tel" name="tel" placeholder="090" value="{{ old('tel') }}" />
+            </div>
+            <div class="form__error">
+              @error('tel')
+              {{ $message }}
+              @enderror
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">住所</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input class="form__input--address" type="email" name="email" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3" value="{{ old('email') }}" />
+            </div>
+            <div class="form__error">
+              @error('email')
+              {{ $message }}
+              @enderror
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">建物名</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input class="form__input--address" type="email" name="email" placeholder="例: 千駄ヶ谷マンション101" value="{{ old('email') }}" />
+            </div>
+            <div class="form__error">
+              @error('email')
+              {{ $message }}
+              @enderror
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お問い合わせ内容</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--textarea">
+              <textarea name="content" placeholder="資料をいただきたいです">{{ old('content') }}</textarea>
+            </div>
+          </div>
+        </div>
+        <div class="form__button">
+          <button class="form__button-submit" type="submit">送信</button>
+        </div>
+      </form>
+    </div>
+@endsection
