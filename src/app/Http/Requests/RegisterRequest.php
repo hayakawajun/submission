45c04d067ee5b68d82be09f73d5ba2987ba31fb2
hayakawa jun <hayakawajugon@gmail.com>
2025-09-05 16:37:08 +0900

    Fortify認能実装

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -13,7 +13,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,26 @@ class RegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required','string','max:255'],
+            'email' => ['required','string','max:255','email','unique:users'],
+            'password' => ['required','string','max:255']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'お名前を入力して下さい',
+            'name.string' => 'お名前を文字列で入力してください',
+            'name.max' => 'お名前は255文字以内で入力してください',
+            'email.required' => 'メールアドレスを入力して下さい',
+            'email.string' => 'メールアドレスを文字列で入力してください',
+            'email.max' => 'メールアドレスは255文字以内で入力してください',
+            'email.email' => 'メールアドレスは「ユーザー名＠ドメイン」形式で入力してください',
+            'email.unique' => '入力したメールアドレスはすでに登録されています',
+            'password.required' => 'パスワードを入力してください',
+            'password.string' => 'パスワードを文字列で入力してください',
+            'password.max' =>'パスワードは255文字以内で入力してください'
         ];
     }
 }
